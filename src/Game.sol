e1dcc1e0cce0cca3f385cde5b3a3127b8805b9b5
Ben Baessler <mail@benbaessler.com>
2024-05-24 13:57:05 +0100

    feat: initial values

--- src/Game.sol
+++ src/Game.sol
@@ -1,6 +1,23 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.26;
+pragma solidity ^0.8.25;
 
-contract Game {
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 
+contract Game is Ownable {
+    using ECDSA for bytes32;
+
+    address public protocolTreasury = 0x0;
+    address public channelHost;
+
+    // 5% protocol fee
+    address public protocolFeePercent = 0.05 ether;
+    // 5% channel host fee (of final prize pool)
+    address public hostFeePercent = 0.05 ether;
+    // 15% winning cast creator fee (of final prize pool)
+    address public creatorFeePercent = 0.15 ether;
+
+    constructor(address _channelHost) Ownable(msg.sender) {
+        channelHost = _channelHost;
+    }
 }
