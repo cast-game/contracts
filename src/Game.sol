8946ab040aacb45ba753693afaab14e2f265fc25
Ben Baessler <mail@benbaessler.com>
2024-05-24 23:17:54 +0100

    feat: complete game contract

--- src/Game.sol
+++ src/Game.sol
@@ -3,32 +3,69 @@ pragma solidity ^0.8.25;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "./Tickets.sol";
 
 contract Game is Ownable {
     using ECDSA for bytes32;
 
     Tickets public tickets;
+    IERC20 public token;
 
     address public protocolTreasury = 0x0;
     address public channelHost;
 
     // 5% protocol fee
     address public protocolFeePercent = 0.05 ether;
+    // 5% creator fee (of transaction)
+    address public creatorFeePercent = 0.05 ether;
     // 5% channel host fee (of final prize pool)
     address public hostFeePercent = 0.05 ether;
     // 15% winning cast creator fee (of final prize pool)
-    address public creatorFeePercent = 0.15 ether;
+    address public winnningCreatorFeePercent = 0.15 ether;
+
+    bool public isActive = false;
+    bool public endBlock;
 
     // Nonce to ensure hashes are unique per transaction
     mapping(string => uint256) public nonce;
 
+    event Purchase(
+        address indexed buyer,
+        string indexed castHash,
+        uint256 amount,
+        uint256 price,
+        uint256 protocolFee,
+        uint256 creatorFee
+    );
+
+    event GameEnded(
+        string indexed castHash,
+        address indexed winningCreator,
+        address[] ticketHolders
+    );
+
     error InsufficientPayment();
     error TransferFailed();
+    error GameNotActive();
 
-    constructor(address _channelHost, address _tickets) Ownable(msg.sender) {
+    constructor(
+        address _channelHost,
+        address _tickets,
+        address _token
+    ) Ownable(msg.sender) {
         tickets = Tickets(_tickets);
+        token = IERC20(_token);
         channelHost = _channelHost;
+    }
+
+    function startGame(uint256 duration) external onlyOwner {
+        isActive = true;
+        endBlock = block.number + duration;
+    }
+
+    function updateGameStatus(bool _isActive) external onlyOwner {
+        isActive = _isActive;
     }
 
     /// @notice recover the signer from the hash and signature
@@ -42,19 +79,21 @@ contract Game is Ownable {
         if (signer != owner()) revert InvalidSignature();
     }
 
-    function buyTickets(
+    function buy(
         string memory castHash,
         address castCreator,
         uint256 amount,
-        uint256 scv,
+        uint256 price,
         bytes memory signature
-    ) external payable {
+    ) external {
+        if (!isActive || block.number > endBlock) revert GameNotActive();
+
         bytes32 hash = keccak256(
             abi.encodePacked(
                 castHash,
                 castCreator,
                 amount,
-                scv,
+                price,
                 nonce[castHash]
             )
         );
@@ -62,22 +101,54 @@ contract Game is Ownable {
         verifySignature(signature, hash);
         nonce[castHash]++;
 
-        // TODO: get price from bonding curve + scv
-        uint256 price = 1 ether;
-
+        // Calculate fees
         uint256 protocolFee = (price * protocolFeePercent) / 1 ether;
         uint256 creatorFee = (price * creatorFeePercent) / 1 ether;
 
         if (msg.value < price + protocolFee + creatorFee)
             revert InsufficientPayment();
 
-        // Transfer the fees
-        (bool success0, ) = protocolTreasury.call{value: protocolFee}("");
-        (bool success1, ) = castCreator.call{value: creatorFee}("");
-        if (!success0 || !success1) revert TransferFailed();
+        // Transfer fees
+        token.transferFrom(msg.sender, protocolTreasury, protocolFee);
+        token.transferFrom(msg.sender, castCreator, creatorFee);
 
-        // TODO: mint ERC1155 token from Tickets.sol
+        // Mint ERC1155
+        tickets.mint(msg.sender, castHash, amount);
 
-        // TODO: emit event
+        emit Purchase(
+            msg.sender,
+            castHash,
+            amount,
+            price,
+            protocolFee,
+            creatorFee
+        );
+    }
+
+    function endGame(
+        string memory castHash,
+        address winningCreator,
+        address[] ticketHolders
+    ) external onlyOwner {
+        // Calculate fees
+        uint256 hostFee = (price * hostFeePercent) / 1 ether;
+        uint256 winningCreatorFee = (price * winnningCreatorFeePercent) /
+            1 ether;
+
+        // Transfer fees
+        token.transferFrom(address(this), channelHost, hostFee);
+        token.transferFrom(address(this), winningCreator, winningCreatorFee);
+
+        uint256 remainingPool = token.balanceOf(address(this));
+        // Distribute the rest of the prize pool to ticket holders
+        for (uint256 i = 0; i < ticketHolders.length; i++) {
+            token.transferFrom(
+                address(this),
+                ticketHolders[i],
+                remainingPool / ticketHolders.length
+            );
+        }
+
+        emit GameEnded(castHash, winningCreator, ticketHolders);
     }
 }
