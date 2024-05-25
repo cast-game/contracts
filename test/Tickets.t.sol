a61a525da87058c1b3f81e34c6d1fe4fdfc2a977
Ben Baessler <mail@benbaessler.com>
2024-05-25 04:34:40 +0100

    test: tickets

---
+++ test/Tickets.t.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.25;
+
+import {Test, console} from "forge-std/Test.sol";
+
+import "../src/Game.sol";
+import "../src/Tickets.sol";
+
+contract TicketsTest is Test {
+    Tickets public tickets;
+
+    function setUp() public {
+        tickets = new Tickets();
+        tickets.setGameContract(address(this));
+    }
+
+    function test_Mint() public {
+        tickets.mint(address(1), "0x1", 1);
+        assertEq(tickets.balanceOf(address(1), 0), 1);
+        assertEq(tickets.castTokenId("0x1"), 0);
+    }
+
+    function testFuzz_Mint(uint256 x) public {
+        tickets.mint(address(1), "0x1", x);
+    }
+
+    function test_SetTokenURI() public {
+        tickets.setTokenUri(1, "https://google.com");
+        assertEq(tickets.uri(1), "https://google.com");
+    }
+}
