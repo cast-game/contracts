65c30b79ff9dce769d258e18724225e56aa2a384
Ben Bassler <mail@benbaessler.com>
2024-07-24 13:06:23 +0100

    chore: update script

--- script/Game.s.sol
+++ script/Game.s.sol
@@ -12,8 +12,6 @@ contract GameScript is Script {
         // test (dev wallet)
         address channelHost = 0xa85B5383e0E82dBa993747834f91FE03FCCD40ab;
         address protocolTreasury = 0xa85B5383e0E82dBa993747834f91FE03FCCD40ab;
-        // test degen (mockerc20)
-        address tokenAddress = 0x6E67E6AE4A1fB0b8c45897ECCfdCE8408B1e1640;
 
         // dev wallet
         uint256 deployerPrivateKey = vm.envUint("DEPLOYER_PRIVATE_KEY");
@@ -24,7 +22,6 @@ contract GameScript is Script {
             "test",
             channelHost,
             address(tickets),
-            tokenAddress,
             protocolTreasury
         );
 
